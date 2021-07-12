c0701895d0c0cb6101aa319df930bc8e37994423
Anuththara <rajananda046@gmail.com>
2021-07-12 12:31:39 +0530

    added new sout to app.java in core module

--- core/src/main/java/app/App.java
+++ core/src/main/java/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello user management core module!" );
+        System.out.println( "Testing cherry-pick command" );
     }
 }
